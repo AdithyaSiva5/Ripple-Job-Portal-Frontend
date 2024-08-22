0e44cc7328a2f48b8245722d8fc83bad4918323d
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-08-22 13:59:47 +0530

    big-fix

--- src/constants/baseUrls.ts
+++ src/constants/baseUrls.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = "https://ripplejobportal.in";
-// export const DOMIAN_URL="https://ripplejobportal.in";
-export const BASE_URL = "http://localhost:3000";
-export const DOMIAN_URL="http://localhost:5173";
+export const BASE_URL = "https://ripplejobportal.in";
+export const DOMIAN_URL = "https://ripplejobportal.in";
+// export const BASE_URL = "http://localhost:3000";
+// export const DOMIAN_URL="http://localhost:5173";
